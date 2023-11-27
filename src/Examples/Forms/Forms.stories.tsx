3d8d331f1654e6a1b5ed39b52029be2025ce038d
Aleksandar Grbic <hi@aleksandar.xyz>
2023-11-27 22:53:45 +0100

    Add autodocs to forms

--- src/Examples/Forms/Forms.stories.tsx
+++ src/Examples/Forms/Forms.stories.tsx
@@ -1,11 +1,12 @@
-import useAjvForm from "@programmer_network/use-ajv-form";
+import useAjvForm from "@programmer_network/use-ajv-form/src/index";
 
 import Button from "Components/Button";
 import { Input, Textarea } from "Components/Inputs";
 
 const meta = {
   title: "Input/Forms",
-  argTypes: {}
+  argTypes: {},
+  tags: ["autodocs"]
 };
 
 export default meta;
@@ -17,50 +18,54 @@ export const UseAjvForm = () => {
       type: "object",
       required: [],
       properties: {
-        title: { type: "string", minLength: 3, "secure-string": true },
+        title: { type: "string", minLength: 3 },
         description: { type: "string", minLength: 5 },
         about: { type: "string", minLength: 5 }
       }
-    }
+    },
+    { shouldDebounceAndValidate: false }
   );
 
   return (
-    <div className='flex flex-col gap-4 p-8'>
-      <div className='grid grid-cols-2 gap-4'>
-        <Input
-          name='title'
-          label='Title'
-          type='text'
-          value={form.state.title.value}
-          error={form.state.title.error as string}
-          onBlur={() => form.onBlur("title")}
-          onChange={({ title }) => form.set({ title })}
+    <div>
+      <div className='flex flex-col gap-4 p-8'>
+        <div className='grid grid-cols-2 gap-4'>
+          <Input
+            name='title'
+            label='Title'
+            type='text'
+            value={form.state.title.value}
+            error={form.state.title.error as string}
+            onBlur={() => form.onBlur("title")}
+            onChange={({ title }) => form.set({ title })}
+          />
+          <Input
+            name='description'
+            label='Description'
+            type='text'
+            value={form.state.description.value}
+            error={form.state.description.error as string}
+            onBlur={() => form.onBlur("description")}
+            onChange={({ description }) => form.set({ description })}
+          />
+        </div>
+        <Textarea
+          name='about'
+          label='About'
+          value={form.state.about.value}
+          error={form.state.about.error as string}
+          onBlur={() => form.onBlur("about")}
+          onChange={({ about }) => form.set({ about })}
         />
-        <Input
-          name='description'
-          label='Description'
-          type='text'
-          value={form.state.description.value}
-          error={form.state.description.error as string}
-          onBlur={() => form.onBlur("description")}
-          onChange={({ description }) => form.set({ description })}
-        />
+        <Button
+          className='max-w-max'
+          onClick={() => form.validate()}
+          disabled={!form.isDirty}
+        >
+          Save
+        </Button>
+        {!form.isValid && <h1 className='text-pink-600'>Invalid Form</h1>}
       </div>
-      <Textarea
-        name='about'
-        label='About'
-        value={form.state.about.value}
-        error={form.state.about.error as string}
-        onBlur={() => form.onBlur("about")}
-        onChange={({ about }) => form.set({ about })}
-      />
-      <Button
-        className='max-w-max'
-        onClick={() => form.validate()}
-        disabled={!form.isDirty}
-      >
-        Save
-      </Button>
     </div>
   );
 };
