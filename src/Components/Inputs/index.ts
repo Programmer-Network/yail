c3b32835ff2d4f753089ab17a824f73b2f8d1e5a
Aleksandar Grbic <hi@aleksandar.xyz>
2023-11-28 22:47:18 +0100

    Export checkbox and bump to 1.0.5

--- src/Components/Inputs/index.ts
+++ src/Components/Inputs/index.ts
@@ -3,3 +3,4 @@ export { default as InputError } from "./Common/InputError";
 export { default as Input } from "./Input";
 export { default as Textarea } from "./Textarea";
 export { default as Select } from "./Select";
+export { default as Checkbox } from "./Checkbox";
