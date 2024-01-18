a5625de270595fbdbd130fbafa4708f5db0c7c77
Aleksandar Grbic <hi@aleksandar.xyz>
2024-01-18 21:51:25 +0100

    Update attribute names

--- src/Components/Icons/IconDrag.tsx
+++ src/Components/Icons/IconDrag.tsx
@@ -8,9 +8,9 @@ const IconDrag: FC<IIconProps> = props => {
       <path
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
         d='M14 18a1 1 0 1 0 2 0a1 1 0 0 0-2 0m-6 0a1 1 0 1 0 2 0a1 1 0 0 0-2 0m6-6a1 1 0 1 0 2 0a1 1 0 0 0-2 0m-6 0a1 1 0 1 0 2 0a1 1 0 0 0-2 0m6-6a1 1 0 1 0 2 0a1 1 0 0 0-2 0M8 6a1 1 0 1 0 2 0a1 1 0 0 0-2 0'
       ></path>
     </svg>
