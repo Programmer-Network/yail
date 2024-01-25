24294ea20a5aef3ce758a20b0eb5dfb5afd72fa0
Aleksandar Grbic <hi@aleksandar.xyz>
2024-01-25 23:03:24 +0100

    Improve viewbox for Heading icons

--- src/Components/Icons/IconH1.tsx
+++ src/Components/Icons/IconH1.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from "react";
 
 const SvgIconH1 = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='3 7 18 10' {...props}>
     <path
       fill='currentColor'
       d='M5 17V7h2v4h4V7h2v10h-2v-4H7v4zm12 0V9h-2V7h4v10z'
--- src/Components/Icons/IconH4.tsx
+++ src/Components/Icons/IconH4.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from "react";
 
 const SvgIconH4 = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='3 7 19 10' {...props}>
     <path
       fill='currentColor'
       d='M3 17V7h2v4h4V7h2v10H9v-4H5v4zm15 0v-3h-5V7h2v5h3V7h2v5h2v2h-2v3z'
