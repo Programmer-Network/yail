37d842a2edfc85b64c127c9e12cdbf0c665e8e37
Aleksandar Grbic <hi@aleksandar.xyz>
2024-02-02 17:48:53 +0100

    Remove text-sm from AuthorCard

--- src/Components/AuthorCard/index.tsx
+++ src/Components/AuthorCard/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Avatar from "Components/Avatar";
-import { Description, Title } from "Components/Typography";
+import { H3, Paragraph } from "Components/Typography";
 
 import { IAuthorCardProps } from "./types";
 
@@ -16,13 +16,13 @@ const AuthorCard: React.FC<IAuthorCardProps> = ({
       className='border border-primary-text-color p-6'
       onClick={() => (onClick ? onClick({ name, avatar, about }) : undefined)}
     >
-      <div className='flex flex-col items-center gap-4 text-sm md:flex-row'>
+      <div className='flex flex-col items-center gap-4 md:flex-row'>
         <div className='hidden md:block'>
           <Avatar src={avatar} size={75} />
         </div>
         <div>
-          <Title>{name}</Title>
-          <Description className='!text-base'>{about}</Description>
+          <H3 className='!mb-0'>{name}</H3>
+          <Paragraph className='!text-base'>{about}</Paragraph>
         </div>
       </div>
     </div>
