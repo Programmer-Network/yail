8be11aa4339527c58daa0909b415a9c1940b5129
Aleksandar Grbic <hi@aleksandar.xyz>
2024-02-03 11:15:28 +0100

    Remove conflicting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
-    "no-unused-vars": "error",
     "@typescript-eslint/no-unused-vars": ["error"]
   }
 };
