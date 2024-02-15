1a4bf943e5d6441f12a224992405c81c70ff10b9
Aleksandar Grbic <hi@aleksandar.xyz>
2024-02-15 21:10:21 +0100

    Update package version and CTA component styles

--- src/Components/Typography/Anchor/index.tsx
+++ src/Components/Typography/Anchor/index.tsx
@@ -2,8 +2,14 @@ import { FC } from "react";
 
 import { IAnchorProps } from "./types";
 
-const Anchor: FC<IAnchorProps> = ({ target, href, children }: IAnchorProps) => (
+const Anchor: FC<IAnchorProps> = ({
+  target,
+  href,
+  children,
+  onClick
+}: IAnchorProps) => (
   <a
+    onClick={onClick}
     className='hover:text-stroke text-primary hover:underline hover:opacity-80 active:opacity-50'
     target={target}
     href={href}
--- src/Components/Typography/Anchor/types.ts
+++ src/Components/Typography/Anchor/types.ts
@@ -2,4 +2,5 @@ export interface IAnchorProps {
   href: string;
   target?: string;
   children?: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
