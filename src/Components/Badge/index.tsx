91553fe8126c349ce85d9233677f98467f8dcd27
Aleksandar Grbic <hi@aleksandar.xyz>
2024-02-20 23:03:09 +0100

    Update package version and remove relative positioning in Badge component

--- src/Components/Badge/index.tsx
+++ src/Components/Badge/index.tsx
@@ -7,7 +7,7 @@ const Badge: FC<IBadgeProps> = ({ title, className }) => {
   return (
     <span
       className={classNames(
-        "relative top-[-2px] rounded border border-primary-text-color px-1 pb-[2px] pt-[1px] text-[10px] text-primary-text-color",
+        "rounded border border-primary-text-color px-1 pb-[2px] pt-[1px] text-[10px] text-primary-text-color",
         className
       )}
     >
