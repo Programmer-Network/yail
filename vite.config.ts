feba0e9b7ec6ba1e77be0b6e91dc25f7e164d764
Aleksandar Grbic <hi@aleksandar.xyz>
2024-03-10 17:23:31 +0100

    Update package.json and tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     lib: {
       entry: "src/index.ts",
       name: "Yail",
-      formats: ["es", "umd"],
+      formats: ["es"],
       fileName: format => `yail.${format}.js`
     },
     rollupOptions: {
