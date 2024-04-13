1bf972ce0729d2b73100862f4a8cf090f0fe04d2
Aleksandar Grbic <hi@aleksandar.xyz>
2024-04-13 21:10:47 +0200

    Don't nest a div inside a paragraph

--- src/Components/ContentWidget/index.tsx
+++ src/Components/ContentWidget/index.tsx
@@ -32,7 +32,7 @@ const ContentWidget = <T extends IContentWidgetBaseItem>({
     >
       <div>
         <div className='group rounded-md'>
-          <Paragraph
+          <div
             className={classNames("items-center gap-2 flex justify-between")}
           >
             {more && more.iconName && (
@@ -43,7 +43,7 @@ const ContentWidget = <T extends IContentWidgetBaseItem>({
                 })}
               />
             )}
-            <span className='font-bold'>{title}</span>
+            <span className='font-bold text-primary-text-color'>{title}</span>
             {more && (
               <Tooltip id='see-more-tooltip' text={more.tooltipText}>
                 <IconEnter
@@ -52,7 +52,7 @@ const ContentWidget = <T extends IContentWidgetBaseItem>({
                 />
               </Tooltip>
             )}
-          </Paragraph>
+          </div>
         </div>
         <Divider
           className='pt-2 pb-3'
