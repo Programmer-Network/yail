ea352b7e977a5d6bfa339b5dcde85dacd5719330
Aleksandar Grbic <hi@aleksandar.xyz>
2024-05-12 09:44:16 +0200

    Export Views

---
+++ src/Views/index.ts
@@ -0,0 +1,3 @@
+export { default as Onboarding } from "./Onboarding";
+export { default as SuccessfulSignup } from "./Authentication/SuccessfulSignup";
+export { default as PasswordChanged } from "./PasswordReset/PasswordChanged";
